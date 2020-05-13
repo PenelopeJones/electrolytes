734db254084ee0f61c64d22d6eec54f4a0b1ff31
Penelope Jones <pj321@login-e-10.data.cluster>
2020-05-13 13:07:51 +0100

    Adding results from HPC.

---
+++ data/results/2520/run1.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+
+mkdir bs020_is020
+cd bs020_is020
+bin_size=0.20
+ion_size=0.20
+
+directory=2.5-20/
+dataset=2520
+num_files=30
+box_length=10.0
+
+min_r_value=0.00
+max_r_value=1.4
+smoothed=True
+
+python ../../../../descriptor_generator.py --directory ${directory} --dataset ${dataset} --num_files ${num_files} \
+    --box_length ${box_length} --bin_size ${bin_size} --ion_size ${ion_size} \
+    --min_r_value ${min_r_value} --max_r_value ${max_r_value} --smoothed ${smoothed}
+
+
+
