b52dca3a279d68bea497c563d9c1620f48fc30b7
Penelope Jones <pj321@login-e-15.data.cluster>
2020-05-14 10:48:55 +0100

    new results

---
+++ data/results/1080/run1.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+
+mkdir bs010_is010
+cd bs010_is010
+bin_size=0.10
+ion_size=0.10
+
+directory=1.0-80/
+dataset=1080
+num_files=49
+box_length=8.0
+
+min_r_value=0.00
+max_r_value=1.25
+smoothed=True
+
+python ../../../../descriptor_generator.py --directory ${directory} --dataset ${dataset} --num_files ${num_files} \
+    --box_length ${box_length} --bin_size ${bin_size} --ion_size ${ion_size} \
+    --min_r_value ${min_r_value} --max_r_value ${max_r_value} --smoothed ${smoothed}
+
+
+
