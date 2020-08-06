7facf1597c2440a3a35b68ff6fd7ceebe576bf4c
PenelopeJones <pj321@cam.ac.uk>
2020-08-06 22:03:02 +0100

    Additional utils files

--- data_utils.py
+++ data_utils.py
@@ -1,6 +1,69 @@
 import numpy as np
+from scipy.stats import norm, invwishart, multivariate_normal
+from numpy.linalg.linalg import inv
+import pdb
 
-from scipy.stats import norm
+
+def all_sampler(wk_invs, vks, betaks, mks, scalers, n_samples):
+    """
+
+    :param wk_invs:
+    :param vks:
+    :param betaks:
+    :param mks:
+    :param scalers:
+    :param n_samples:
+    :return:
+    """
+
+    n_trials = len(betaks)
+    K = betaks[0].shape[0]
+
+    means = []
+    stds = []
+
+    for k in range(K):
+        all_samples = []
+        for trial in range(n_trials):
+            samples = []
+            inv_lambda_k = invwishart.rvs(df=vks[trial][k], scale=wk_invs[trial][k], size=n_samples)
+            for sample in range(n_samples):
+                mu_k = multivariate_normal.rvs(mean=mks[trial][k],
+                                               cov=np.reciprocal(betaks[trial][k]) * inv_lambda_k[sample], size=1)
+                x = multivariate_normal.rvs(mean=mu_k, cov=inv_lambda_k[sample], size=1)
+                samples.append(x)
+            all_samples.append(scalers[trial].inverse_transform(np.array(samples)))
+        all_samples = np.array(all_samples).reshape(-1, 8)
+        means.append(np.mean(all_samples, axis=0))
+        stds.append((np.std(all_samples, axis=0)))
+    means = np.array(means)
+    stds = np.array(stds)
+    return means, stds
+
+
+def sampler(wk_inv, vk, betak, mk, scaler, n_samples):
+    """
+    :param k: int
+    :param wk: [K, dim, dim]
+    :param vk: [K,]
+    :param betak: [K,]
+    :param mk: [K, dim]
+    :return:
+    """
+    K = vk.shape[0]
+    means = []
+    stds = []
+    for k in range(K):
+        samples = []
+        inv_lambda_k = invwishart.rvs(df=vk[k], scale=wk_inv[k], size=n_samples)
+        for sample in range(n_samples):
+            mu_k = multivariate_normal.rvs(mean=mk[k], cov=np.reciprocal(betak[k]) * inv_lambda_k[sample], size=1)
+            x = multivariate_normal.rvs(mean=mu_k, cov=inv_lambda_k[sample], size=1)
+            samples.append(x)
+        samples = scaler.inverse_transform(np.array(samples))
+        means.append(np.mean(samples, axis=0))
+        stds.append(np.std(samples, axis=0))
+    return means, stds
 
 
 def midpoints(min_r_value, max_r_value, bin_size):
@@ -13,9 +76,10 @@ def midpoints(min_r_value, max_r_value, bin_size):
     :param bin_size: The histogram bin size (float).
     :return: vector of midpoint values.
     """
-    min_r = min_r_value + 0.5*bin_size
-    max_r = max_r_value - 0.5*bin_size
+    min_r = min_r_value + 0.5 * bin_size
+    max_r = max_r_value - 0.5 * bin_size
     return np.arange(min_r, max_r, bin_size)
+
 
 def radial_distance(x_i, y_i, z_i, x_j, y_j, z_j, box_length):
     """
@@ -34,7 +98,7 @@ def radial_distance(x_i, y_i, z_i, x_j, y_j, z_j, box_length):
     delta_x = min(((x_i - x_j) % box_length), ((x_j - x_i) % box_length))
     delta_y = min(((y_i - y_j) % box_length), ((y_j - y_i) % box_length))
     delta_z = min(((z_i - z_j) % box_length), ((z_j - z_i) % box_length))
-    return np.sqrt(delta_x**2 + delta_y**2 + delta_z**2)
+    return np.sqrt(delta_x ** 2 + delta_y ** 2 + delta_z ** 2)
 
 
 def rdf(r, prefactor, bin_size, ion_size, min_r_value, max_r_value, smoothed=False):
@@ -77,7 +141,8 @@ def rdf(r, prefactor, bin_size, ion_size, min_r_value, max_r_value, smoothed=Fal
 
 def dists(ion_A, prefactor_a, ion_B=None, prefactor_b=None, ion_C=None,
           prefactor_c=None, smoothed=False, bin_size=0.15,
-          ion_size=0.15, min_r_value=0.0, max_r_value=1.0, box_length=12.0):
+          ion_size=0.15, min_r_value=0.0, max_r_value=1.0, box_length=12.0,
+          xcol=2, ycol=3, zcol=4):
     """
     Given up to three pandas dataframes each comprising the 3D co-ordinates of all
     ions of a particular type (at a particular snapshot in time), this calculates the
@@ -107,8 +172,8 @@ def dists(ion_A, prefactor_a, ion_B=None, prefactor_b=None, ion_C=None,
         for i, rows_Aa in ion_A.iterrows():
             if i == j:
                 continue
-            x = radial_distance(ion_A.at[j, 2], ion_A.at[j, 3], ion_A.at[j, 4], ion_A.at[i, 2],
-                                ion_A.at[i, 3], ion_A.at[i, 4], box_length)
+            x = radial_distance(ion_A.at[j, xcol], ion_A.at[j, ycol], ion_A.at[j, zcol], ion_A.at[i, xcol],
+                                ion_A.at[i, ycol], ion_A.at[i, zcol], box_length)
             r_a_j.append(x)
         r_a_j = np.asarray(r_a_j)
         g_a = rdf(r_a_j, prefactor_a, bin_size, ion_size, min_r_value, max_r_value, smoothed)
@@ -116,8 +181,8 @@ def dists(ion_A, prefactor_a, ion_B=None, prefactor_b=None, ion_C=None,
 
         if ion_B is not None:
             for k, rows_B in ion_B.iterrows():
-                x = radial_distance(ion_A.at[j, 2], ion_A.at[j, 3], ion_A.at[j, 4], ion_B.at[k, 2],
-                                    ion_B.at[k, 3],ion_B.at[k, 4], box_length)
+                x = radial_distance(ion_A.at[j, xcol], ion_A.at[j, ycol], ion_A.at[j, zcol], ion_B.at[k, xcol],
+                                    ion_B.at[k, ycol], ion_B.at[k, zcol], box_length)
                 r_b_j.append(x)
             r_b_j = np.asarray(r_b_j)
             g_b = rdf(r_b_j, prefactor_b, bin_size, ion_size, min_r_value, max_r_value, smoothed)
@@ -125,8 +190,8 @@ def dists(ion_A, prefactor_a, ion_B=None, prefactor_b=None, ion_C=None,
 
         if ion_C is not None:
             for l, rows_C in ion_C.iterrows():
-                x = radial_distance(ion_A.at[j, 2], ion_A.at[j, 3], ion_A.at[j, 4], ion_C.at[l, 2],
-                                    ion_C.at[l, 3], ion_C.at[l, 4], box_length)
+                x = radial_distance(ion_A.at[j, xcol], ion_A.at[j, ycol], ion_A.at[j, zcol], ion_C.at[l, xcol],
+                                    ion_C.at[l, ycol], ion_C.at[l, zcol], box_length)
                 r_c_j.append(x)
             r_c_j = np.asarray(r_c_j)
             g_c = rdf(r_c_j, prefactor_c, bin_size, ion_size, min_r_value, max_r_value, smoothed)
